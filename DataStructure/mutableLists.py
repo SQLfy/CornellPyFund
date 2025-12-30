847ce92a0acfff901e54ca0660a9b04b5d3a3a54
Kate Totten <katetotten@gmail.com>
2025-12-30 13:08:39 -0700

    files from data structures class

---
+++ DataStructure/mutableLists.py
@@ -0,0 +1,65 @@
+"""
+Module with mutable functions on lists.
+
+All of these functions modify their list arguments.
+
+Author: Kate Totten
+Date: 2025-12-29
+"""
+
+
+def clamp(alist,min,max):
+    """
+    Modifies alist so that every element is between min and max.
+    
+    Any number in the list less than min is replaced with min.  Any number
+    in the tuple greater than max is replaced with max. Any number between
+    min and max is left unchanged.
+    
+    Examples:
+        If a = [-1, 1, 3, 5], clamp(a,0,4) changes a to [0,1,3,4]
+        If a = [-1, 1, 3, 5], clamp(a,-2,8) changes a to [-1,1,3,5]
+        If a = [-1, 1, 3, 5], clamp(a,-2,-1) changes a to [-1,-1,-1,-1]
+        If a = [], clamp(a,0,4) returns []
+    
+    Parameter alist: the list to modify
+    Precondition: alist is a list of numbers (float or int)
+    
+    Parameter min: the minimum value for the list
+    Precondition: min <= max is a number
+    
+    Parameter max: the maximum value for the list
+    Precondition: max >= min is a number
+    """
+    #print(alist)
+    #print(f'min {min} and max {max}')
+    for i in range(len(alist)):
+        if alist[i] < min:
+            alist[i] = min
+        elif alist[i] > max:
+            alist[i] = max
+        #else value is acceptable
+
+def removeall(alist,n):
+    """
+    Removes all instances of n from alist
+    
+    Examples:
+        If a = [1,2,2,3,1], removeall(a,1) changes a to [2,2,3]
+        If a = [1,2,2,3,1], removeall(a,2) changes a to [1,3,1]
+        If a = [1,2,2,3,1], removeall(a,4) changes a to [1,2,2,3,1]
+        If a = [1,1,1], removeall(a,1) changes a to []
+        If a = [], removeall(a,1) changes a to []
+    
+    Parameter alist: the list to modify
+    Precondition: alist is a list of numbers (float or int)
+    
+    Parameter n: the number to remove
+    Precondition: n is a number
+    """
+    try:
+        for i in range(len(alist) - 1, -1, -1):
+            if alist[i] == n:
+                alist.remove(alist[i])
+    except:
+        print('Not happening: ran out of range')
